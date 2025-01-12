7f3c2c1071b71f04495a298fd97649d265c7c38f
sbuthaud <jrdm-001@MBP-SB-2022.local>
2025-01-12 09:42:17 +0100

    Correction des chemins d'importation pour Render

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+# Ajout du répertoire du projet au PYTHONPATH
+project_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, project_dir)
+
 from miniblog import create_app
 
 app = create_app()
